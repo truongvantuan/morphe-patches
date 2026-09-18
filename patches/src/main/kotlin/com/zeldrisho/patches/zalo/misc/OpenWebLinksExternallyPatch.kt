12d5bb607b20fd840454e1a2ffdcb87c31865dc8
tvtuan.dev <truongvantuan@outlook.com.vn>
2026-09-18 10:00:31 +0700

    fix(zalo): correct fingerprint for OpenWebLinksExternallyPatch

--- patches/src/main/kotlin/com/zeldrisho/patches/zalo/misc/OpenWebLinksExternallyPatch.kt
+++ patches/src/main/kotlin/com/zeldrisho/patches/zalo/misc/OpenWebLinksExternallyPatch.kt
@@ -3,14 +3,17 @@ package com.zeldrisho.patches.zalo.misc
 import app.morphe.patcher.Fingerprint
 import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
 import app.morphe.patcher.patch.bytecodePatch
+import app.morphe.patcher.methodCall
 import com.android.tools.smali.dexlib2.AccessFlags
 
 private object OpenWebLinksExternallyFingerprint : Fingerprint(
     definingClass = "Lcom/zing/zalo/ui/zviews/vt;",
     name = "k",
     returnType = "V",
-    parameters = listOf("L", "Ljava/lang/String;", "Landroid/os/Bundle;", "Z", "I", "L"),
     accessFlags = listOf(AccessFlags.STATIC),
+    filters = listOf(
+        methodCall(definingClass = "Lcom/zing/zalo/ui/zviews/ZaloWebView;", name = "A8")
+    ),
 )
 
 val openWebLinksExternallyPatch = bytecodePatch(
