856339ae1957dd116ce88d60cb518214768c63fc
tvtuan.dev <truongvantuan@outlook.com.vn>
2026-09-18 10:06:43 +0700

    fix(zalo): replace fingerprint with structural pattern matching for web links

--- patches/src/main/kotlin/com/zeldrisho/patches/zalo/misc/OpenWebLinksExternallyPatch.kt
+++ patches/src/main/kotlin/com/zeldrisho/patches/zalo/misc/OpenWebLinksExternallyPatch.kt
@@ -1,20 +1,8 @@
 package com.zeldrisho.patches.zalo.misc
 
-import app.morphe.patcher.Fingerprint
 import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
 import app.morphe.patcher.patch.bytecodePatch
-import app.morphe.patcher.methodCall
 import com.android.tools.smali.dexlib2.AccessFlags
-
-private object OpenWebLinksExternallyFingerprint : Fingerprint(
-    definingClass = "Lcom/zing/zalo/ui/zviews/vt;",
-    name = "k",
-    returnType = "V",
-    accessFlags = listOf(AccessFlags.STATIC),
-    filters = listOf(
-        methodCall(definingClass = "Lcom/zing/zalo/ui/zviews/ZaloWebView;", name = "A8")
-    ),
-)
 
 val openWebLinksExternallyPatch = bytecodePatch(
     name = "Open web links externally",
@@ -22,15 +10,34 @@ val openWebLinksExternallyPatch = bytecodePatch(
 ) {
     compatibleWith(com.zeldrisho.patches.zalo.shared.Constants.COMPATIBILITY_ZALO)
     execute {
-        OpenWebLinksExternallyFingerprint.method.addInstructions(
-            0,
-            """
-            invoke-static {p0, p1, p2}, Lcom/zeldrisho/zalo/extension/WebLinkHelper;->tryOpenExternal(Ljava/lang/Object;Ljava/lang/String;Landroid/os/Bundle;)Z
-            move-result v0
-            if-eqz v0, :continue
-            return-void
-            :continue
-            """.trimIndent(),
-        )
+        classDefForEach { classDef ->
+            if (classDef.type == "Lcom/zing/zalo/ui/zviews/vt;") {
+                val mutableClass = mutableClassDefBy(classDef)
+                
+                val bindMethod = mutableClass.methods.firstOrNull {
+                    it.name == "k" && 
+                    it.returnType == "V" && 
+                    AccessFlags.STATIC.isSet(it.accessFlags) &&
+                    it.parameterTypes.size == 6 &&
+                    it.parameterTypes[1] == "Ljava/lang/String;" &&
+                    it.parameterTypes[2] == "Landroid/os/Bundle;" &&
+                    it.parameterTypes[3] == "Z" &&
+                    it.parameterTypes[4] == "I"
+                }
+
+                if (bindMethod != null) {
+                    bindMethod.addInstructions(
+                        0,
+                        """
+                        invoke-static {p0, p1, p2}, Lcom/zeldrisho/zalo/extension/WebLinkHelper;->tryOpenExternal(Ljava/lang/Object;Ljava/lang/String;Landroid/os/Bundle;)Z
+                        move-result v0
+                        if-eqz v0, :continue
+                        return-void
+                        :continue
+                        """.trimIndent(),
+                    )
+                }
+            }
+        }
     }
 }
